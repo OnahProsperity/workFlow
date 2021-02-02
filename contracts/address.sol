1df459955ea8fb0e9d8e8c16ba642bfb87cea00e
Prosperity <prosperauthor@gmail.com>
2021-02-02 06:22:34 +0100

    getting start on how to add,remove, get of address.

--- contracts/address.sol
+++ contracts/address.sol
@@ -1,18 +1,24 @@
 pragma solidity ^0.8.0;
 
 contract AddressBook {
+    // storage
     mapping(address => address[]) private _addresses;
     mapping(address => mapping(address => string)) private _aliases;
 
+    // getting a particular address from the storage
     function getAddresses() public view returns (address[]) {
         return _addresses[msg.sender];
     }
 
+    // adding adress and name of the address owner into the storage
     function addAddress(address addr, string alias) public {
         _addresses[msg.sender].push(addr);
         _aliases[msg.sender][addr] = alias;
     }
 
+    // removing a particualar address from storage
+    // later on we will set an admin to have the license to remove a particular address from this contract
+    // if need for this is urgent and it not forth coming kindly request
     function removeAddress(address addr) public {
         uint length = _addresses[msg.sender].length;
         for(uint i = 0; i < length; i++) {
@@ -28,6 +34,7 @@ contract AddressBook {
         }
     }
 
+    // getting an address by name from storage.
     function getAlias(address addr) public view returns (string) {
         return _aliases[msg.sender][addr];
     }
